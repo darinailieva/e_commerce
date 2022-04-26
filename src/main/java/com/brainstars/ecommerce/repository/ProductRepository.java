c772dfdc6bc3b1160e13b6c7f1db4e6647c92fdc
Darina Ilieva <darina.ilieva@gmail.com>
2022-04-26 17:18:30 +0300

    Changed the repository to comply of new sorting logic.

--- src/main/java/com/brainstars/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/brainstars/ecommerce/repository/ProductRepository.java
@@ -2,8 +2,8 @@ package com.brainstars.ecommerce.repository;
 
 import com.brainstars.ecommerce.models.Product;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
@@ -11,9 +11,36 @@ import java.util.List;
  * JPA repository for Products.
  */
 
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-    @Query("FROM Product p where p.name like %:name% ORDER BY p.name")
-    List<Product> findAllByOrderByName(String name, Pageable pageable);
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
+    @Query("FROM Product p ORDER BY p.name ASC")
+    List<Product> findAllOrderByNameASC(String name, Pageable pageable);
+
+    @Query("FROM Product p ORDER BY p.name DESC")
+    List<Product> findAllOrderByNameDESC(String name, Pageable pageable);
+
+    @Query("FROM Product p ORDER BY p.quantity DESC")
+    List<Product> findAllOrderByQuantityDESC(String orderBy, Pageable pageable);
+
+    @Query("FROM Product p ORDER BY p.quantity ASC")
+    List<Product> findAllOrderByQuantityASC(String orderBy, Pageable pageable);
+
+    @Query("FROM Product p ORDER BY p.createdDate DESC")
+    List<Product> findAllOrderByCreatedDateDESC(String orderBy, Pageable pageable);
+
+    @Query("FROM Product p ORDER BY p.createdDate ASC")
+    List<Product> findAllOrderByCreatedDateASC(String orderBy, Pageable pageable);
+
+    @Query("FROM Product p ORDER BY p.lastModifiedDate DESC")
+    List<Product> findAllOrderByLastModifiedDateDESC(String orderBy, Pageable pageable);
+
+    @Query("FROM Product p ORDER BY p.lastModifiedDate ASC")
+    List<Product> findAllOrderByLastModifiedDateASC(String orderBy, Pageable pageable);
+
+    @Query("FROM Product p ORDER BY p.id DESC")
+    List<Product> getAllDESC();
+
+    @Query("FROM Product p ORDER BY p.id ASC")
+    List<Product> getAllASC();
 
     @Query("select p.category, count(p.category) From Product p group By p.category")
     List<Object[]> findAllByCategories();
